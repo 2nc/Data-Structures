07fa35fac8653a5217452094b0f504c3a836b189
2nc <wnchit@163.com>
2019-06-03 12:43:28 +0800

    Check style of Intlist.java

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -11,11 +11,11 @@ public class IntList {
     /**
      * First element of list.
      */
-    public int first;
+    private int first;
     /**
      * Remaining elements of list.
      */
-    public IntList rest;
+    private IntList rest;
 
     /**
      * A List with first FIRST0 and rest REST0.
@@ -81,12 +81,11 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        if(A == null){
+        if (A == null) {
             return B;
         }
         IntList temp = A;
-        while(temp.rest != null){
+        while (temp.rest != null) {
             temp = temp.rest;
         }
         temp.rest = B;
@@ -98,11 +97,10 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        if(A == null){
+        if (A == null) {
             return B;
         }
-        return  new IntList(A.first,catenate(A.rest,B));
+        return  new IntList(A.first, catenate(A.rest, B));
     }
 
 

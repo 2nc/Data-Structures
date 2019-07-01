28b805cdbb119af4f92718cf2b0e7ef8a60c3b2b
2nc <wnchit@163.com>
2019-07-01 22:54:16 +0800

    lab3

---
+++ lab3/Flik/Flik.java
@@ -0,0 +1,7 @@
+/** An Integer tester created by Flik Enterprises. */
+public class Flik {
+    public static boolean isSameNumber(Integer a, Integer b) {  //passing number except -128 to 127 will not equal for not caching
+       // System.out.print( a == b );
+        return a == b;
+    }
+}
